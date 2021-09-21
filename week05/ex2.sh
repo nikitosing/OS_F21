6b3027842d4d5c290e09d67251bdf6973db2b94f
nikitosing <nikitosing4@mail.ru>
2021-09-21 23:35:28 +0300

    Week 05 lab

---
+++ week05/ex2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+file="ex2.txt"
+lock_file="${file}.lock"
+while true:
+do
+if [ ! -f $file ]
+then
+  echo 0 > $file
+fi
+
+while [ -f $lock_file ]
+do
+  :
+done
+
+echo "locked" > $lock_file
+
+count=$(tail -n 1 $file)
+count=count+1
+echo $count >> $file
+
+rm $lock_file
+done 
