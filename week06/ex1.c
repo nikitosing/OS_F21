dd95580b7d139e195b54a735c10a22e31b252ed0
nikitosing <nikitosing4@mail.ru>
2021-09-28 21:25:22 +0300

    Week 06 Lab

--- week06/ex1.c
+++ week06/ex1.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 struct proccess{
-	unsigned int arrival_time;
-	unsigned int burst_time;
+	int arrival_time;
+	int burst_time;
+	int id;
 };
 
-void swap(struct proccess * a_, struct proccess * b_){
-	struct proccess t = *a_;
-	*a_ = *b_;
-	*b_ = t;
+void swap(struct proccess* arr, int a, int b){
+	struct proccess t = arr[a];
+	arr[a] = arr[b];
+	arr[b] = t;
 }
 
 void bubble_sort_by_arrival_time(struct proccess *array, int size){
 	for (int i = 0; i < size - 1; i ++) {
 		for (int j = i + 1; j < size; j ++) {
 			if (array[j].arrival_time < array[i].arrival_time) {
-				swap(&array[j], &array[i]);
+				swap(array, i, j);
 			}
 		}
 	}		
@@ -23,7 +25,7 @@ void bubble_sort_by_arrival_time(struct proccess *array, int size){
 
 
 int main(){
-	unsigned int n;
+	int n;
 	
 	printf("Input number of a proccesses: ");
 	scanf("%d", &n);
@@ -31,26 +33,34 @@ int main(){
 	struct proccess array[n];
 	
 	printf("Input elements of the array\n");
-	for (int i = 1; i <= n; i++) {
-		printf("Input arrival_time for [%u]: ", i);
+	for (int i = 0; i < n; i++) {
+		printf("Input arrival_time for [%d]: ", i);
 		scanf("%d", &array[i].arrival_time);
-		printf("Input burst_time for [%u]: ", i);
+		printf("Input burst_time for [%d]: ", i);
 		scanf("%d", &array[i].burst_time);
+		array[i].id = i + 1;
 	}
 	
 	bubble_sort_by_arrival_time(array, n);
 	
 	printf("P#\t AT\t BT\t CT\t TAT\t WT\n\n");
-	
-	unsigned int wait_time = 0;
-	for (int i = 1; i <= n; i++) {
-		printf("P#%d\t %u\t %u\t %u\t %u\t %u\n", 
-			i, 
+	int tat = 0;
+	int wt = 0;
+	int wait_time = 0;
+	for (int i = 0; i < n; i++) {
+		tat = wait_time + array[i].burst_time - array[i].arrival_time;
+		if (tat < 0)
+			tat = 0;
+		wt = wait_time - array[i].arrival_time;
+		if (wt < 0)
+			wt = 0;
+		printf("P#%d\t %d\t %d\t %d\t %d\t %d\n", 
+			array[i].id, 
 			array[i].arrival_time, 
 			array[i].burst_time, 
 			wait_time + array[i].burst_time, 
-			wait_time + array[i].burst_time - array[i].arrival_time,
-			wait_time - array[i].arrival_time);
+			tat,
+			wt);
 		wait_time += array[i].burst_time;
 	}
 	
