ab94e304b9d0a675e3401fa6c3914f4b18a6e1af
nikitosing <nikitosing4@mail.ru>
2021-10-12 22:48:30 +0300

    Week 08 Lab

---
+++ week08/ex4.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/resource.h>
+/*
+The only thing was changed during the execution time is a free memory in `vmstat` info. `si` and `so` are 0.
+*/
+
+/*
+If we are looking at the output of `top`. We can see, that VIRT and RES for our proccess grow as fast as program alocating new memory
+*/
+
+int main() {
+    void* ptr;
+    long to_allocate = 10 * 1024 * 1024;
+    for (int i = 0; i < 10; ++i) {
+        ptr = malloc(to_allocate);
+        memset(ptr, 0, to_allocate);
+        printf("%d MB allocated\n", (i+1)*10);
+		printf("%d used", getrusage());
+        sleep(1);
+    }
+    return 0;
+}
