35bea42d929bb6263857e79f5569fc4466576f6d
nikitosing <nikitosing4@mail.ru>
2021-10-12 22:23:54 +0300

    Week 08 Lab

--- week08/ex2.c
+++ week08/ex2.c
@@ -7,11 +7,11 @@
 int main() {
     void* ptr;
     long to_allocate = 10 * 1024 * 1024;
-    for (int i = 0; i < 10; ++i) {
+    for (int i = 0; i < 100; ++i) {
         ptr = malloc(to_allocate);
         memset(ptr, 0, to_allocate);
-        printf("%d MB allocated", (i+1)*10);
-        sleep(1);
+        printf("%d MB allocated\n", (i+1)*10);
+        sleep(0.5);
     }
     return 0;
 }
