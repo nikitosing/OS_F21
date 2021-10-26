cd70a064a7cb22a941f9dc3300bd30b6a5627188
nikitosing <nikitosing4@mail.ru>
2021-10-26 22:31:50 +0300

    Week 10 Lab

--- week10/ex4.c
+++ week10/ex4.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/dir.h>
 
 int main(){
-	dirp = opendir("tmp");
+	DIR	= opendir("tmp");
 	if (dirp == NULL){ 
 		return 0;
 	}
