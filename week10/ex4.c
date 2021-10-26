1c199824d88b94778675f3867dce5156e1edea59
nikitosing <nikitosing4@mail.ru>
2021-10-26 22:40:52 +0300

    Week 10 Lab

--- week10/ex4.c
+++ week10/ex4.c
@@ -13,9 +13,9 @@ int main(){
 	struct dirent* dp;
 	while ((dp = readdir(dirp)) != NULL ) {
 		struct stat de_stat;
-		stat(dp->d_name, &dir_entry_stat);
-		if (de_stat->st_nlink > 1) {
-			printf("%d %d %s", dp->d_ino, de_stat->st_nlink, dp->d_name);
+		stat(dp->d_name, &de_stat);
+		if (de_stat.st_nlink > 1) {
+			printf("%d %d %s", dp->d_ino, de_stat.st_nlink, dp->d_name);
 		}
 	}
 	(void) closedir(dirp);
