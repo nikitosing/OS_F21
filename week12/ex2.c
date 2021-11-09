38fd980e7a54c614e8c616f73faf9210d145b55a
nikitosing <nikitosing4@mail.ru>
2021-11-09 22:29:07 +0300

    Week 12 Lab

--- week12/ex2.c
+++ week12/ex2.c
@@ -8,7 +8,9 @@ int main(){
 	struct input_event ev;
 	ssize_t n;
 	int fd;
+	
 	fd = open(file, O_RDONLY);
+	
 	if (fd == -1){
 		printf("Error access");
 		return -1;
@@ -16,7 +18,14 @@ int main(){
 	
 	while (1){
 		n = read(fd, &ev, sizeof ev);
-		printf("type: %x, %d",ev.code, ev.type);
-	}
+		//printf("type: %x, %d",ev.code, ev.type);
+		if (ev.type == 1) {
+			if (ev.value == 1){
+				printf("PRESSED %x (%i)", ev.code, ev.code);
+			} else if (ev.value == 1){
+				printf("RELEASED %x (%i)", ev.code, ev.code);
+			}
+		}
+	} 
 	return 0;
 }
